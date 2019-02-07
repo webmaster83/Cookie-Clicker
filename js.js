f3f6cf3e9b4274eb64f5e6161bc5dc33b70574a4
webmaster83 <nachit.n@gmail.com>
2019-02-07 14:55:24 +0100

    etape 8 Done :-)

--- js.js
+++ js.js
@@ -2,8 +2,19 @@
 //var manipuler divv
 //var score = 0 ;
 
-var score = 0;
-var multiplicateur = 1;
+
+/*document.getElementById("clic").addEventListener("click", function() {
+  score += multiplicateur ;
+  document.getElementById("affichage").innerHTML = score;
+
+
+  })
+
+  document.getElementById("multiplier").addEventListener("click", function(augmenterMultiplicateur) {
+    multiplicateur ++;
+    console.log(multiplicateur);
+
+})*/
 
 
 
@@ -13,13 +24,23 @@ $(document).ready( function(){
 	var multiplicateur = 1;
 	
 	$("#clic").click(function(){
-		score+= multiplicateur;
+		score += multiplicateur;
 		$("#affichage").text(score);
 	});
 	
-	$("#mutiplier").click(function(){
-		multiplicateur++;
-		console.log(multiplicateur);
+	$("#multiplier").click(function(){
+		multiplicateur ++;
+		//console.log(multiplicateur);
+		if ( score >= 10 ){
+		score = score - 10;
+		$("#affichage").text(score);
+		
+	} 
+		
+	
+		$("#multiplier").text( " Multiplicateur x"+ multiplicateur);
+	
 	});
+	
 	
 });
