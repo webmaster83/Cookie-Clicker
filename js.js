5d5d83a3b551eaae61722287c775226be03d23d0
webmaster83 <nachit.n@gmail.com>
2019-02-07 09:37:48 +0100

    structure modifier sans CDN jquery

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
 
+
 (function() {
-
-
 
 var score = 0;
 
 document.getElementById("clic").addEventListener("click", function() {
   score ++;
 document.getElementById("affichage").innerHTML = score;
-})
+});
 })();
