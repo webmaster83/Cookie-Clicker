7656b4b0164d5bc617a0e1ffce350460690cb05a
webmaster83 <nachit.n@gmail.com>
2019-02-07 10:08:05 +0100

    Le jeux en Jquery

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+//var manipuler divv
+//var score = 0 ;
+
+$(document).ready( function(){
+	
+	var score = 0 ;
+	$("#clic").click(function(){
+		score++;
+		$("#affichage").text(score);
+	});
+});
