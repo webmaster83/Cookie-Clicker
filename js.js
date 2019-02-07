094e06297c5ed8ee972f05092ef0ddf31ee30f97
caterinamennito <caterina.mennito@gmail.com>
2019-02-07 10:08:18 +0100

    step 1

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+
+(function() {
+
+
+
+var score = 0;
+
+document.getElementById("clic").addEventListener("click", function() {
+  score ++;
+document.getElementById("affichage").innerHTML = score;
+})
+})();
