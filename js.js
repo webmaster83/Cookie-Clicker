585580fca6f978d30d073e67f942ba2fcbcd3743
webmaster83 <nachit.n@gmail.com>
2019-02-07 15:49:06 +0100

    Etape 10 Done

--- js.js
+++ js.js
@@ -3,16 +3,19 @@
 //var score = 0 ;
 
 
-/*document.getElementById("clic").addEventListener("click", function() {
-  score += multiplicateur ;
-  document.getElementById("affichage").innerHTML = score;
+/*
 
-
-  })
 
   document.getElementById("multiplier").addEventListener("click", function(augmenterMultiplicateur) {
-    multiplicateur ++;
-    console.log(multiplicateur);
+    if (score - 10 * multiplicateur < 0){
+      alert("Tu n'as pas assez de crédit!");
+    }
+    else {
+      score = score - 10 * (multiplicateur);
+      document.getElementById("affichage").innerHTML = score;
+      multiplicateur ++;
+      console.log(multiplicateur);
+      document.getElementById("multiplier").innerHTML = "multiplier X" + multiplicateur + " Coût prochain achat: " + 10 * multiplicateur;
 
 })*/
 
@@ -29,25 +32,21 @@ $(document).ready( function(){
 	});
 	
 	$("#multiplier").click(function(){
-		multiplicateur ++;
+		
 		//console.log(multiplicateur);
-		if ( score >= 10 ){
-		score = score - 10;
-		$("#affichage").text(score);
-			
-			
-		var clicks = $(this).data("clicks");
-			if (clicks){
-				score = score - 10;
-				
-			} else {
-				score = (score - 10)*2;
-			}
-		 
-	} 
+		if ( score - 10 * multiplicateur < 0 ){
+			$("#affichage").text("Tu ne peux pas car le score sera NEGATIF. :-) ");	 
+						} else {
+			score = score - 10 * multiplicateur;	// sinon on garde notre calcul ( le le fait )
+			$("#affichage").text(score); // affiche
+			multiplicateur ++;	// on incremente +1
+			console.log(multiplicateur);
+			$("#multiplier").text( " Multiplicateur x"+ multiplicateur+ ' cout prochain achat ' + 10 * multiplicateur );
+	
+						}
 		
 	
-		$("#multiplier").text( " Multiplicateur x"+ multiplicateur);
+		//$("#multiplier").text( " Multiplicateur x"+ multiplicateur);
 	
 	});
 	
