23f5402a9e2a0426e4ff4a6b5e46ffbac1e0f047
caterinamennito <caterina.mennito@gmail.com>
2019-02-07 11:50:29 +0100

    multiplier

--- js.js
+++ js.js
@@ -2,11 +2,23 @@
 (function() {
 
 
-
 var score = 0;
+var multiplicateur = 1;
 
 document.getElementById("clic").addEventListener("click", function() {
-  score ++;
-document.getElementById("affichage").innerHTML = score;
+  score += multiplicateur ;
+  document.getElementById("affichage").innerHTML = score;
+
+
+  })
+
+  document.getElementById("multiplier").addEventListener("click", function(augmenterMultiplicateur) {
+    multiplicateur ++;
+    console.log(multiplicateur);
+
 })
+
+
+
+
 })();
