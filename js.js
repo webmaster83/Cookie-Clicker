3b17151ed6d2140f024909475f96d82f5ddf1d46
caterinamennito <caterina.mennito@gmail.com>
2019-02-08 12:09:47 +0100

    update

--- js.js
+++ js.js
@@ -3,42 +3,78 @@
 var score = 0;
 
 var multiplicateur = 1;
-var autoclick = setInterval(function(){
-  if (score >= 10) {
-    score += multiplicateur ;
-    document.getElementById("affichage").innerHTML = score;
-  }
-}, 1000);
+
+var conteurAutoclic = 0;
 
 document.getElementById("autoclic").disabled = true;
+document.getElementById("bonus").disabled = true;
 
-document.getElementById("autoclic").addEventListener("click", function(){
+
+
+  document.getElementById("autoclic").addEventListener("click", function(){
+
+  conteurAutoclic ++;
   score -= 5;
+  console.log(conteurAutoclic);
+
+  var autoclick = setInterval(function(){
+    if (score >= 10) {
+      score += multiplicateur ;
+      document.getElementById("affichage").innerHTML = score;
+    }
+  }, 1000);
+
+
 	document.getElementById("autoclic").disabled = true;
   document.getElementById("affichage").innerHTML = score;
-})
+}, {once : true});
+
+
 
 document.getElementById("clic").addEventListener("click", function() {
   score += multiplicateur ;
   if (score>= 5){
+    if (conteurAutoclic<1) {
   document.getElementById("autoclic").disabled = false;
   }
+  else {  document.getElementById("autoclic").disabled = true;
+ }
+}
+
+if (score>=10){
+  document.getElementById("bonus").disabled = false;
+
+  document.getElementById("bonus").addEventListener("click", function() {
+      score -= 10;
+    document.getElementById("bonus").disabled = true;
+    document.getElementById("affichage").innerHTML = score;
+
+
+  }
+  )
+}
+
   document.getElementById("affichage").innerHTML = score;
 
 
   })
 
+
+
   document.getElementById("multiplier").addEventListener("click", function(augmenterMultiplicateur) {
     if (score - 10 * multiplicateur < 0){
-      alert("Tu n'as pas assez de crédit!");
-    }
-    else {
-      score = score - 10 * (multiplicateur);
-      document.getElementById("affichage").innerHTML = score;
-      multiplicateur ++;
-      console.log(multiplicateur);
-      document.getElementById("multiplier").innerHTML = "multiplier X" + multiplicateur + " Coût prochain achat: " + 10 * multiplicateur;
+     alert("Tu n'as pas assez de crédit!");
+   }
+   else {
+     score = score - 10 * (multiplicateur);
+     document.getElementById("affichage").innerHTML = score;
+     multiplicateur ++;
+     console.log(multiplicateur);
+     document.getElementById("multiplier").innerHTML = "multiplier X" + multiplicateur + " Coût prochain achat: " + 10 * multiplicateur;
 }
+
+
+
 
 
 
