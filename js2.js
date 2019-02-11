a6624959d1df68277603c1fb547fb88d94c69d96
webmaster83 <nachit.n@gmail.com>
2019-02-11 12:02:56 +0100

    jusque là ça marche très bien sauf affiche 10 direct .....

--- js2.js
+++ js2.js
@@ -4,19 +4,47 @@
 
 var score = 0 ;
 var multiplicateur = 1;
+var cout = 10;
+/* desactive le bouton*/
+$("#multiplier").attr("disabled","disabled");
 
 /*etape 3*/
-$("#affichage").text(score);
+/*$("#affichage").text(score);*/
+
 $("#clic").click( function(){
-	score ++ ;
+	score += multiplicateur ;
 	$("#affichage").text(score);
+	
+	
+	/*si le score vaut au moins 20 ou 50*/
+	if  (score - cout*multiplicateur >= 0) { /*  20 au lieu 50 */
+	$("#multiplier").removeAttr("disabled");
+	
+		
+	}
+	else {
+		$("#multiplier").attr("disabled","disabled");
+		/**console.log(" NON ! votre résultat sera NEGATIF :-) ");*/
+		
+	}
+	
+	
 });
 
-/*etape4 et 5 */
+
+/*etape4 et 5  */
 $("#multiplier").click(function(){
-	var = nouveauscore ;
+	
+	var coco = cout*multiplicateur ; 
 	
 	multiplicateur ++ ; 
-	score += multiplicateur ; 
-	$("affichage").text(score);
+	
+	score = score - 10 ;/* etape 6*/
+	$("#affichage").text(score);
+	
+	$("#multiplier").text("Multiplicateur X " + multiplicateur + "(" +  coco  + ")"   );
+	/*etape 7 nouvelle valeur afficher */  /*etape 8 la phrase*/ 
+	
 });
+
+
