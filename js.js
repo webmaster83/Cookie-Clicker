15673adc7ff80ea58a91e04fbf37350237751af2
caterinamennito <caterina.mennito@gmail.com>
2019-02-12 10:45:32 +0100

    bonus problem fixed

--- js.js
+++ js.js
@@ -3,49 +3,54 @@
     var score = 0;
     var multiplicateur = 1;
     var autoClic = 0;
-    var prix = 10;
-    var timeleft = 10;
-    var newMult= multiplicateur * 2;
+    var prix = 50;
+    var timeleft = 30;
 
 
+//Au chargement de la page, on vérifie si les conditions pour activer les boutons sont remplies
+    window.onload = function() {
+      checkMult();
+      checkAuto();
+      checkBonus();
 
-document.getElementById("autoclic").disabled = true;
-document.getElementById("bonus").disabled = true;
-document.getElementById("multiplier").disabled = true;
+    };
 
 
+//Vérifie si on a les conditions nécessaires pour activer le bouton MULTIPLIER
 function checkMult (){
   if (score - prix>= 0) {
     document.getElementById("multiplier").disabled = false;
   }
+  else{
+    document.getElementById("multiplier").disabled = true;
+  }
 };
 
+//Vérifie si on a les conditions nécessaires pour activer le bouton AUTOCLIC
 function checkAuto (){
-  if (score >= 5 && autoClic===0){
+  if (score >= 500 && autoClic===0){
     document.getElementById("autoclic").disabled = false;
   }
+  else{
+    document.getElementById("autoclic").disabled = true;
+  }
 }
 
+//Vérifie si on a les conditions nécessaires pour activer le bouton BONUS
 function checkBonus (){
-  if (score >= 20){
+  if (score >= 5000 && timeleft>=30){
     document.getElementById("bonus").disabled = false;
-  }
+}else{
+  document.getElementById("bonus").disabled = true;
+}
 }
 
-/*function doubleMult(){
-  if (timeleft >= 0) {
-    score += newMult;
-  }
-    else {
-  		clearInterval(timer);
-  		//multiplicateur = multiplicateur/2;
-  	}
-}
-*/
+
+
 //COOKIE
   document.getElementById("clic").addEventListener("click", function() {
       score += multiplicateur ;
-      document.getElementById("affichage").innerHTML = score;
+      document.getElementById("affichage").innerHTML = parseInt(score);
       checkBonus();
       checkAuto();
       checkMult();
@@ -57,14 +62,12 @@ document.getElementById("multiplier").addEventListener("click", function(augment
 
   document.getElementById("multiplier").disabled = true;
    score = score - prix ;
-   document.getElementById("affichage").innerHTML = score;
+   document.getElementById("affichage").innerHTML = parseInt(score);
    multiplicateur ++;
    prix = prix * 2;
    document.getElementById("multiplier").innerHTML = "multiplier X" + multiplicateur + " Coût prochain achat: " + prix;
-   console.log(prix);
    checkBonus();
    checkAuto();
-   checkMult();
 
 });
 
@@ -76,13 +79,13 @@ document.getElementById("multiplier").addEventListener("click", function(augment
 document.getElementById("autoclic").addEventListener("click", function(){
 
 autoClic ++;
-score -= 5;
+score -= 500;
 console.log(autoClic);
 
  setInterval(function(){
-  if (score >= 10) {
+  if (score >= 200) {
     score += multiplicateur ;
-    document.getElementById("affichage").innerHTML = score;
+    document.getElementById("affichage").innerHTML = parseInt(score);
     checkBonus();
     checkMult();
 
@@ -90,11 +93,10 @@ console.log(autoClic);
 }, 1000);
 
 checkBonus();
-checkAuto();
 checkMult();
 
 document.getElementById("autoclic").disabled = true;
-document.getElementById("affichage").innerHTML = score;
+document.getElementById("affichage").innerHTML = parseInt(score);
 }, {once : true});
 
 
@@ -102,33 +104,33 @@ document.getElementById("affichage").innerHTML = score;
 //BONUS
 document.getElementById("bonus").addEventListener("click", function() {
 
-  score = score - 10;
   document.getElementById("bonus").disabled = true;
+  score = score - 5000;
 
-      /*  function desactivateBon(){
-          if (timeleft>=0){
+  var timer = setInterval(function(){
+    document.getElementById('bonus').innerHTML= "BONUS - " + timeleft;
+    timeleft -= 1;
+    checkBonus();
 
-      }
-*/
-var timer = setInterval(function(){
-  document.getElementById('bonus').innerHTML= "BONUS - " + timeleft;
-  timeleft -= 1;
-  if (timeleft <= 0) {
+//vérifie si les 30 secondes se sont écoulées
+//et fait en sorte que tout revienne à la "normalité"
+  if (timeleft == 0) {
     clearInterval(timer);
     document.getElementById('bonus').innerHTML= "BONUS";
     multiplicateur = multiplicateur/2;
+    timeleft = 30;
+    checkBonus();
+    checkMult();
   }
 }, 1000);
 
 
 multiplicateur = multiplicateur * 2;
 
-
-checkBonus();
 checkAuto();
 checkMult();
 
-document.getElementById("affichage").innerHTML = score;
+document.getElementById("affichage").innerHTML = parseInt(score);
 
 
 
